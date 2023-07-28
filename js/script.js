30d9059e18c9b60631b53659e2c879368e353995
Adam Britt <adamrussellbritt@gmail.com>
2023-07-28 15:44:04 -0500

    finished project

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const topicElement = document.querySelector(".topic");
+const countElement = document.querySelector(".count");
+
+const learning = {};
+learning.topic = "JS";
+learning.learningGoals = ["Build JS Programs", "Learn jQuery", "Learn React", "Practice New Frameworks"];
+learning.category = "Front End Development";
+learning.topicImportance = "High";
+
+// console.log(learning);
+
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1,1);
+
+// console.log(learning);
+
+topicElement.innerText = `I'm learning ${learning.topic}.`;
+topicElement.classList.remove("hide");
+countElement.innerText = `I have ${learning.learningGoals.length} learning goals.`;
+countElement.classList.remove("hide");
